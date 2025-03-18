66343161a2036e9f465ae8821124185d7d840415
mohtmuss <mussaop812@gmail.com>
2025-03-18 11:16:32 -0400

    Update hello.js

--- Lab1/js/hello.js
+++ Lab1/js/hello.js
@@ -1,16 +1,24 @@
+
 var today = new Date();
 var hourNow = today.getHours();
+var minutesNow = today.getMinutes();
+var secondsNow = today.getSeconds();
 var greeting;
-if(hourNow > 18){
+
+
+if (hourNow > 18) {
     greeting = 'Good evening';
-}
-else if(greeting > 12){
-     greeting = 'Good afternoon';
-}else if(greeting > 0 ){
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon';
+} else if (hourNow > 0) {
     greeting = 'Good morning';
-}else{
-    greeting = 'Wellcome'
+} else {
+    greeting = 'Welcome';
 }
-document.write("<br>");
- document.write(greeting);
- 
+
+
+document.write(greeting);
+document.write("<br>"); 
+
+
+document.write("Current time: " + hourNow + ":" + minutesNow + ":" + secondsNow);
